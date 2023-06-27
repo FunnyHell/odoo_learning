479508d072b2e791a21b3c425eb2913ea604b62c
FunnyHell <sofunnyhell@gmail.com>
2023-06-27 19:59:46 +0300

    Second module

--- market-learn/models/seller.py
+++ market-learn/models/seller.py
@@ -4,8 +4,8 @@ class Seller(models.Model):
     _name = 'market.seller'
     _description = 'Seller of products'
 
-    name = fields.String(string='Name', required=True)
+    name = fields.Char(string='Name', required=True)
     gender = fields.Selection([
-        'male', 'Male',
-        'female', 'Female'
+        ('male', 'Male'),
+        ('female', 'Female')
     ], default='male')
