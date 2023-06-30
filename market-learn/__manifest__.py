e7b2840e66c3d9522cdd7677191c8186068f627b
FunnyHell <sofunnyhell@gmail.com>
2023-06-30 21:59:38 +0300

    Second module

--- market-learn/__manifest__.py
+++ market-learn/__manifest__.py
@@ -1,5 +1,10 @@
 {
-    'name' : 'My own market module',
-    'author' : 'Sergey Omelianenko',
-    'summary' : 'Odoo 15 learning module on simple market example'
+    'name': 'My own market module',
+    'author': 'Sergey Omelianenko',
+    'summary': 'Odoo 15 learning module on simple market example',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/products.xml'
+    ]
 }
