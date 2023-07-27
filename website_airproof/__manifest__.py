b20ef5ac26daa6d2549684c956891b5f70e3a8e4
FunnyHell <sofunnyhell@gmail.com>
2023-07-27 20:27:05 +0300

    small changes

--- website_airproof/__manifest__.py
+++ website_airproof/__manifest__.py
@@ -6,14 +6,18 @@
     'version': '15.0.0',
     'depends': ['website'],
     'data': [
-        # ...
+        'views/website_templates.xml',
     ],
     'assets': {
         'web._assets_primary_variables': [
             ('append', 'website_airproof/static/src/scss/primary_variables.scss'),
         ],
         'web._assets_frontend_helpers': [
-            ('prepend', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
+            ('append', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
+        ],
+        'web.assets_frontend': [
+            'website_airproof/static/src/scss/theme.scss',
+            'website_airproof/static/src/js/theme.js',
         ],
     },
 }
